8926adb66f3c1e164d2d9110de7d506183563c9b
devAndre-Isaac <andreisaac344@gmail.com>
2021-11-22 17:55:20 -0300

    feat: :label: initial types

---
+++ types/tipos.js
@@ -0,0 +1,17 @@
+var idade = 27;
+idade = 27.2;
+console.log(idade);
+var possuiHobbies = false;
+//possuiHobbies = 1
+console.log(possuiHobbies);
+var minhaIdade;
+minhaIdade = 27;
+console.log(typeof minhaIdade);
+// minhaIdade = '27'
+// console.log(typeof minhaIdade)]
+//aray
+var hobbies = ["cozinha", "Praticar Esportes"];
+console.log(hobbies[0]);
+console.log(typeof hobbies);
+hobbies = [100, 200, 300];
+console.log(hobbies);
---
+++ types/tipos.ts
@@ -0,0 +1,32 @@
+let idade: number = 27
+
+idade = 27.2
+console.log(idade)
+
+let possuiHobbies: boolean = false
+
+//possuiHobbies = 1
+
+console.log(possuiHobbies)
+
+let minhaIdade: number
+minhaIdade = 27
+console.log(typeof minhaIdade)
+// minhaIdade = '27'
+// console.log(typeof minhaIdade)]
+
+
+//aray
+let hobbies: any[] = [ "cozinha", "Praticar Esportes"]
+console.log(hobbies[0])
+console.log(typeof hobbies)
+
+hobbies = [100, 200, 300]
+
+console.log(hobbies)
+
+//tuplas
+let endereco: [string, number] = ["av principal", 99]
+console.log(endereco)
+
+endereco = ['a', 0]
