d3c708005db4416e4ea6d5d71a0848e64356a37e
devAndre-Isaac <andreisaac344@gmail.com>
2021-11-23 18:06:23 -0300

    feat: challenge object types and function types

--- types/tipos.js
+++ types/tipos.js
@@ -1,3 +1,5 @@
+"use strict";
+exports.__esModule = true;
 var idade = 27;
 idade = 27.2;
 console.log(idade);
@@ -18,7 +20,7 @@ console.log(hobbies);
 //tuplas
 var endereco = ["av principal", 99];
 console.log(endereco);
-endereco = ['a', 0];
+endereco = ["a", 0];
 //enums
 var Cor;
 (function (Cor) {
@@ -34,12 +36,12 @@ console.log(minhaCor);
 console.log(Cor.Azul);
 console.log(Cor.Laranja, Cor.Amarelo);
 console.log(Cor.Verde, Cor.Vermelho);
-var nome = 'andre';
+var nome = "andre";
 //any
-var carro = 'BMW';
+var carro = "BMW";
 console.log(carro);
 carro = {
-    marca: 'bmw',
+    marca: "bmw",
     ano: 2019
 };
 console.log(carro);
@@ -48,4 +50,50 @@ function retornaMeuNome() {
     return nome;
 }
 console.log(retornaMeuNome());
+function digaOi() {
+    console.log("Oi");
+}
+digaOi();
+function multiplicar(numA, numB) {
+    return numA * numB;
+}
+console.log(multiplicar(2, 2));
+//tipo função
+var test = function (a, b) {
+    return false;
+};
+var calculo;
+// calculo = digaOi
+// calculo()
+calculo = multiplicar;
+console.log(calculo(5, 6));
+//objetos
+var usuario = {
+    nome: "joao",
+    idade: 27
+};
+console.log(usuario);
+// usuario = {}
+// usuario = {
+//     name: 'maria',
+//     age: 12
+// }
+usuario = {
+    nome: "Tide",
+    idade: 22
+};
+var funcionario = {
+    supervisores: ["Ana", "Feranndo"],
+    baterPonto: function (horario) {
+        if (horario <= 8) {
+            return "Ponto Normal";
+        }
+        else {
+            return "fora do horario";
+        }
+    }
+};
+console.log(funcionario.supervisores);
+console.log(funcionario.baterPonto(8));
+console.log(funcionario.baterPonto(9));
 
--- types/tipos.ts
+++ types/tipos.ts
@@ -1,91 +1,128 @@
-import { textSpanEnd } from "typescript"
+import { textSpanEnd } from "typescript";
 
-let idade: number = 27
+let idade: number = 27;
 
-idade = 27.2
-console.log(idade)
+idade = 27.2;
+console.log(idade);
 
-let possuiHobbies: boolean = false
+let possuiHobbies: boolean = false;
 
 //possuiHobbies = 1
 
-console.log(possuiHobbies)
+console.log(possuiHobbies);
 
-let minhaIdade: number
-minhaIdade = 27
-console.log(typeof minhaIdade)
+let minhaIdade: number;
+minhaIdade = 27;
+console.log(typeof minhaIdade);
 // minhaIdade = '27'
 // console.log(typeof minhaIdade)]
 
-
 //aray
-let hobbies: any[] = [ "cozinha", "Praticar Esportes"]
-console.log(hobbies[0])
-console.log(typeof hobbies)
+let hobbies: any[] = ["cozinha", "Praticar Esportes"];
+console.log(hobbies[0]);
+console.log(typeof hobbies);
 
-hobbies = [100, 200, 300]
+hobbies = [100, 200, 300];
 
-console.log(hobbies)
+console.log(hobbies);
 
 //tuplas
-let endereco: [string, number] = ["av principal", 99]
-console.log(endereco)
+let endereco: [string, number] = ["av principal", 99];
+console.log(endereco);
 
-endereco = ['a', 0]
+endereco = ["a", 0];
 
 //enums
 enum Cor {
-    Cinza, ///0
-    Verde = 100,
-    Azul = 2,
-    Laranja, //2
-    Amarelo,
-    Vermelho = 100
+  Cinza, ///0
+  Verde = 100,
+  Azul = 2,
+  Laranja, //2
+  Amarelo,
+  Vermelho = 100,
 }
 
-let minhaCor: Cor = Cor.Verde
-console.log(minhaCor)
-console.log(Cor.Azul)
-console.log(Cor.Laranja, Cor.Amarelo)
-console.log(Cor.Verde, Cor.Vermelho)
+let minhaCor: Cor = Cor.Verde;
+console.log(minhaCor);
+console.log(Cor.Azul);
+console.log(Cor.Laranja, Cor.Amarelo);
+console.log(Cor.Verde, Cor.Vermelho);
 
-let nome = 'andre'
+let nome = "andre";
 //any
-let carro: any = 'BMW'
-console.log(carro)
+let carro: any = "BMW";
+console.log(carro);
 carro = {
-    marca: 'bmw',
-    ano: 2019
-}
-console.log(carro)
+  marca: "bmw",
+  ano: 2019,
+};
+console.log(carro);
 
 //funçoes
-function retornaMeuNome(): string{
-    return nome
+function retornaMeuNome(): string {
+  return nome;
 }
-console.log(retornaMeuNome())
+console.log(retornaMeuNome());
 
-function digaOi(): void{
-    console.log('Oi')
-}
-
-digaOi()
-
-function multiplicar(numA: number, numB: number): number{
-    return numA * numB
+function digaOi(): void {
+  console.log("Oi");
 }
 
-console.log(multiplicar(2, 2))
+digaOi();
 
+function multiplicar(numA: number, numB: number): number {
+  return numA * numB;
+}
+
+console.log(multiplicar(2, 2));
 
 //tipo função
 const test = function (a: number, b: number): boolean {
-    return false
-}
+  return false;
+};
 
-let calculo: (numeroA: number, numeroB: number) => number
+let calculo: (numeroA: number, numeroB: number) => number;
 // calculo = digaOi
 // calculo()
 
-calculo = multiplicar
-console.log(calculo(5, 6))
+calculo = multiplicar;
+console.log(calculo(5, 6));
+
+//objetos
+
+let usuario: { nome: string; idade: number } = {
+  nome: "joao",
+  idade: 27,
+};
+
+console.log(usuario);
+
+// usuario = {}
+
+// usuario = {
+//     name: 'maria',
+//     age: 12
+// }
+
+usuario = {
+  nome: "Tide",
+  idade: 22,
+};
+
+let funcionario: {
+  supervisores: string[];
+  baterPonto: (horas: number) => string;
+} = {
+  supervisores: ["Ana", "Feranndo"],
+  baterPonto(horario: number): string {
+    if (horario <= 8) {
+      return "Ponto Normal";
+    } else {
+      return "fora do horario";
+    }
+  },
+};
+
+console.log(funcionario.supervisores)
+console.log(funcionario.baterPonto(8))
+console.log(funcionario.baterPonto(9))
