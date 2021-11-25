2246c65247346172a38a9a56872b2490552ea512
devAndre-Isaac <andreisaac344@gmail.com>
2021-11-25 16:21:01 -0300

    feat: noImplicityAny

--- compilador/compilador.ts
+++ compilador/compilador.ts
@@ -13,3 +13,12 @@ console.log(nome);
 (function(){
     let nome: string =  'Ana'
 })
+
+
+function soma(a: any, b: any){
+    return a+b
+}
+
+let qualquerCoisa
+qualquerCoisa = 12
+qualquerCoisa = 'abc'
