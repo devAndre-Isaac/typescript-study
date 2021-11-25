185da4838f25297d2287b17c56bc7c2b972437fb
devAndre-Isaac <andreisaac344@gmail.com>
2021-11-25 16:07:01 -0300

    feat: noEmitOnError

---
+++ compilador/compilador.js
@@ -0,0 +1,9 @@
+var canal = 'Gaveta';
+var inscritos = 610234;
+// canal = inscritos
+console.log(canal);
+var nome = 'pedro'; /// variavel redeclarada
+console.log(nome);
+(function () {
+    var nome = 'Ana';
+});
---
+++ compilador/compilador.ts
@@ -0,0 +1,15 @@
+let canal: string = 'Gaveta'
+
+let inscritos: number = 610234
+
+// canal = inscritos
+
+console.log(canal)
+
+let nome = 'pedro'; /// variavel redeclarada
+
+console.log(nome);
+
+(function(){
+    let nome: string =  'Ana'
+})
