e954053ec2ef0e576a3d9333a5cdf869b9110bad
devAndre-Isaac <andreisaac344@gmail.com>
2021-11-26 17:49:06 -0300

    feat: let and const

---
+++ ecmascript/ecmascripts.js
@@ -0,0 +1,11 @@
+///let & const 
+var seraQuePode = '?'; // hoisting
+console.log(seraQuePode);
+var estaFrio = true;
+if (estaFrio) {
+    var acao = 'colocarCasaco';
+    console.log(acao);
+}
+var cpf = '1234567678';
+// cpf = '21243'
+console.log(cpf);
---
+++ ecmascript/ecmascripts.ts
@@ -0,0 +1,17 @@
+///let & const 
+let seraQuePode: string = '?' // hoisting
+console.log(seraQuePode)
+
+let estaFrio = true
+
+if(estaFrio){
+    let acao = 'colocarCasaco'
+    console.log(acao)
+}
+
+const cpf: string = '1234567678'
+
+// cpf = '21243'
+
+console.log(cpf)
+
