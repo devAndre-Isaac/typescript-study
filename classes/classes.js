c67a7bc61498c8c92ed77e81f1642102fd77f69b
devAndre-Isaac <andreisaac344@gmail.com>
2021-12-01 18:44:55 -0300

    feat: classes e atributos 2

--- classes/classes.js
+++ classes/classes.js
@@ -16,3 +16,23 @@ console.log(aniversario);
 var casamento = new Data; // posso omitir os ()
 casamento.ano = 2017;
 console.log(casamento);
+var DataEsperta = /** @class */ (function () {
+    //Público por padrão
+    function DataEsperta(dia, mes, ano) {
+        if (dia === void 0) { dia = 1; }
+        if (mes === void 0) { mes = 1; }
+        if (ano === void 0) { ano = 1970; }
+        this.dia = dia;
+        this.mes = mes;
+        this.ano = ano;
+        this.dia = dia;
+    }
+    return DataEsperta;
+}());
+var aniversario2 = new DataEsperta(3, 11, 1202);
+aniversario.dia = 4;
+console.log(aniversario2.dia);
+console.log(aniversario2);
+var casamento2 = new DataEsperta; // posso omitir os ()
+casamento2.ano = 2017;
+console.log(casamento2);
--- classes/classes.ts
+++ classes/classes.ts
@@ -20,3 +20,21 @@ console.log(aniversario)
 const casamento = new Data // posso omitir os ()
 casamento.ano = 2017
 console.log(casamento)
+
+
+class DataEsperta {
+    //Público por padrão
+    constructor(public dia: number = 1, public mes: number = 1, public ano: number = 1970){
+        this.dia = dia
+    }
+}
+
+
+const aniversario2 = new DataEsperta(3,11, 1202)
+aniversario.dia = 4
+console.log(aniversario2.dia)
+console.log(aniversario2)
+
+const casamento2 = new DataEsperta // posso omitir os ()
+casamento2.ano = 2017
+console.log(casamento2)
