5d0a5e7888bc5a59283c3d075b9eeac2450ce704
devAndre-Isaac <andreisaac344@gmail.com>
2021-12-05 19:46:25 -0300

    feat: namespace em multiplos arquivos

---
+++ namespace/geometriaCirc.js
@@ -0,0 +1,11 @@
+var Geometria;
+(function (Geometria) {
+    var Area;
+    (function (Area) {
+        var PI = 3.14;
+        function circunferencia(raio) {
+            return PI * Math.pow(raio, 2);
+        }
+        Area.circunferencia = circunferencia;
+    })(Area = Geometria.Area || (Geometria.Area = {}));
+})(Geometria || (Geometria = {}));
--- namespace/namespace.js
+++ namespace/namespace.js
@@ -1,18 +1,3 @@
-var Geometria;
-(function (Geometria) {
-    var Area;
-    (function (Area) {
-        var PI = 3.14;
-        function circunferencia(raio) {
-            return PI * Math.pow(raio, 2);
-        }
-        Area.circunferencia = circunferencia;
-        function retangulo(base, altura) {
-            return base * altura;
-        }
-        Area.retangulo = retangulo;
-    })(Area = Geometria.Area || (Geometria.Area = {}));
-})(Geometria || (Geometria = {}));
 var PI = 2.99;
 console.log(Geometria.Area.circunferencia(10));
 console.log(Geometria.Area.retangulo(10, 20));
