65bbfc7064c302922aeb2f79bc5be740d819835a
devAndre-Isaac <andreisaac344@gmail.com>
2021-12-05 19:49:39 -0300

    feat: namespace imports

--- namespace/namespace.js
+++ namespace/namespace.js
@@ -1,3 +1,5 @@
+///<reference path="geometriaCirc.ts"/>
+///<reference path="geometriaRect.ts"/>
 var PI = 2.99;
 console.log(Geometria.Area.circunferencia(10));
 console.log(Geometria.Area.retangulo(10, 20));
