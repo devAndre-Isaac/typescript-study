cfd47585054ed534e8269dda5af6c54e84c3898a
devAndre-Isaac <andreisaac344@gmail.com>
2021-12-07 04:16:28 -0300

    feat: Usando generics

--- genericos/genericos.js
+++ genericos/genericos.js
@@ -4,3 +4,10 @@ function echo(obj) {
 console.log(echo('Joao').lenght);
 console.log(echo(25));
 console.log(echo({ nome: 'Joao' }));
+//Usando generics
+function echoMelhorado(obj) {
+    return obj;
+}
+console.log(echoMelhorado('Joao').length);
+console.log(echoMelhorado(25));
+console.log(echoMelhorado({ nome: 'Joao' }).nome);
--- genericos/genericos.ts
+++ genericos/genericos.ts
@@ -5,3 +5,11 @@ function echo(obj: any){
 console.log(echo('Joao').lenght)
 console.log(echo(25))
 console.log(echo({nome: 'Joao'}))
+
+//Usando generics
+function echoMelhorado<T>(obj: T): T{
+    return obj
+}
+console.log(echoMelhorado('Joao').length)
+console.log(echoMelhorado<number>(25))
+console.log(echoMelhorado({nome: 'Joao'}).nome)
