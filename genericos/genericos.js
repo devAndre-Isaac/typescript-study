15ee2edbf6e7900f0cae70e9ca687bf5ae640d48
devAndre-Isaac <andreisaac344@gmail.com>
2021-12-07 04:10:42 -0300

    feat: introdução

---
+++ genericos/genericos.js
@@ -0,0 +1,6 @@
+function echo(obj) {
+    return obj;
+}
+console.log(echo('Joao').lenght);
+console.log(echo(25));
+console.log(echo({ nome: 'Joao' }));
---
+++ genericos/genericos.ts
@@ -0,0 +1,7 @@
+function echo(obj: any){
+    return obj
+}
+
+console.log(echo('Joao').lenght)
+console.log(echo(25))
+console.log(echo({nome: 'Joao'}))
