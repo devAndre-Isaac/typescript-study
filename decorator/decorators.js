509142c5898c4a3fa1cd9a245b4f8ec715fed900
devAndre-Isaac <andreisaac344@gmail.com>
2021-12-08 20:34:21 -0300

    feat: decorator factory

--- decorator/decorators.js
+++ decorator/decorators.js
@@ -5,15 +5,26 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+// @logarClasse
+// @decorator({a: 'test', b: 2} )
 let Eletrodomestico = class Eletrodomestico {
     constructor() {
         console.log('novo');
     }
 };
 Eletrodomestico = __decorate([
-    logarClasse
+    logarClasseSe(false)
 ], Eletrodomestico);
 function logarClasse(construtor) {
     console.log(construtor);
+}
+function decoratorVazio(_) { }
+function logarClasseSe(valor) {
+    return valor ? logarClasse : null;
+}
+function decorator(obj) {
+    return function (constructor) {
+        console.log(obj.a + ' ' + obj.b);
+    };
 }
 //# sourceMappingURL=decorators.js.map
--- decorator/decorators.ts
+++ decorator/decorators.ts
@@ -1,4 +1,6 @@
-@logarClasse
+// @logarClasse
+// @decorator({a: 'test', b: 2} )
+@logarClasseSe(false)
 class Eletrodomestico {
     constructor(){
         console.log('novo')
@@ -7,4 +9,16 @@ class Eletrodomestico {
 
 function logarClasse(construtor: Function){
     console.log(construtor)
+}
+
+function decoratorVazio(_: Function){}
+
+function logarClasseSe(valor: boolean){
+    return valor? logarClasse : null
+}
+
+function decorator(obj: {a:string, b: number}){
+    return function(constructor: Function): void{
+        console.log(obj.a + ' ' + obj.b)
+    }
 }
