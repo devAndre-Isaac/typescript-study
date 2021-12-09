f8fbd7406c6b3905de24de9134c2d8df3aefef5a
devAndre-Isaac <andreisaac344@gmail.com>
2021-12-09 19:15:10 -0300

    feat: Desafio Decorator Classe

--- decorator/decorators.ts
+++ decorator/decorators.ts
@@ -48,3 +48,30 @@ function imprimivel(construtor: Function){
 // (<any>new Eletrodomestico()).imprimir()
 const eletro = new Eletrodomestico().imprimir()
 eletro.imprimir() && eletro.imprimir()
+
+
+// Desafio Decorator perfilAdmin
+const usuarioLogado = {
+    nome: 'Guilherme Filho',
+    email: 'guigui@gmail.com',
+    admin: true
+}
+@perfilAdmin
+class MudancaAdministrativa {
+    critico() {
+        console.log('Algo crítico foi alterado!')
+    }
+}
+ 
+new MudancaAdministrativa().critico()
+
+function perfilAdmin<T extends Construtor>(construtor: T){
+    return class extends construtor {
+        constructor(...args: any[]){
+            super(...args)
+            if(!usuarioLogado || !usuarioLogado.admin){
+                throw new Error('Sem permissão!')
+            }
+        }
+    }
+}
